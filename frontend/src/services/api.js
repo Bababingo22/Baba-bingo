868a61e148e149d986ac4e917caa76e54f5fd7bc
Bababingo22 <bababingo22@gmail.com>
2025-08-18 17:09:51 +0300

    Create frontend/src/services/api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000/api";
+
+const instance = axios.create({
+  baseURL: API_BASE,
+});
+
+export function setToken(token) {
+  if (token) {
+    instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete instance.defaults.headers.common["Authorization"];
+  }
+}
+
+export default instance;
