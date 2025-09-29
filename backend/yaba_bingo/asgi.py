34a308a9fa5906edcfc39b7141554d6836912289
Bababingo22 <bababingo22@gmail.com>
2025-09-30 00:12:07 +0300

    Update asgi.py

--- backend/yaba_bingo/asgi.py
+++ backend/yaba_bingo/asgi.py
@@ -1,25 +1,19 @@
-backend/yaba_bingo/asgi.py
-
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-Move Django setup to the top
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yaba_bingo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vlad_bingo.settings")
 django.setup()
-
-Now it is safe to import other modules that depend on Django settings
 
 from channels.auth import AuthMiddlewareStack
 from bingo import routing as bingo_routing
 
 application = ProtocolTypeRouter({
-"http": get_asgi_application(),
-"websocket": AuthMiddlewareStack(
-URLRouter(
-bingo_routing.websocket_urlpatterns
-)
-),
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            bingo_routing.websocket_urlpatterns
+        )
+    ),
 })
