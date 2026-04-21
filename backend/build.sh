cb55c046d53a504e33d05b2d62aef7cc449c981e
Bababingo22 <bababingo22@gmail.com>
2026-04-21 19:15:42 +0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -7,6 +7,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 echo "--- FORCING DATABASE TO READ EDITED CARDS ---"
-python manage.py shell -c "import importlib.util; spec = importlib.util.spec_from_file_location('seed', 'bingo/migrations/0004_final_200_cards.py'); module = importlib.util.module_from_spec(spec); spec.loader.exec_module(module); module.seed_200_perfect_cards(None, None); print('SUCCESS: CARDS UPDATED PERFECTLY')"
+python manage.py shell -c "import importlib.util; spec = importlib.util.spec_from_file_location('seed', 'bingo/migrations/0004_final_200_cards.py'); module = importlib.util.module_from_spec(spec); spec.loader.exec_module(module); from bingo.models import PermanentCard; module.seed_200_perfect_cards(None, None)"
 
 python manage.py create_superuser_from_env
