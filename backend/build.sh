25278ce271f7473d76a26a4a09843c8b0b16a42b
Bababingo22 <bababingo22@gmail.com>
2026-04-22 14:12:21 +0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,9 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
 
-echo "--- FORCING DATABASE TO READ EDITED CARDS ---"
-python manage.py shell -c "import importlib.util; spec = importlib.util.spec_from_file_location('seed', 'bingo/migrations/0004_final_200_cards.py'); module = importlib.util.module_from_spec(spec); spec.loader.exec_module(module); from bingo.models import PermanentCard; module.seed_200_perfect_cards(None, None)"
+# This will naturally trigger our new 0005 migration
+python manage.py migrate
 
 python manage.py create_superuser_from_env
